6f3fd0453c8e62c3b52057067ae7dc2de290f0e8
Rodrigo da Silva Ferreira <rodrigomadureira1@outlook.com>
2021-04-29 14:34:35 -0300

    main

---
+++ ex074.py
@@ -0,0 +1,6 @@
+from random import randint
+numeros = randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10)
+print(f'Os números escolhidos foram {numeros}')
+
+print(f'O maior número foi {max(numeros)}')
+print(f'O menor número foi {min(numeros)}')
