1a973d3db7ec0bc7b79c6c3027cbfe828f5560c4
Rodrigo da Silva Ferreira <rodrigomadureira1@outlook.com>
2021-07-29 20:15:32 -0300

    main

--- ex084.py
+++ ex084.py
@@ -1,11 +1,35 @@
 lista = []
+pesados = []
+leves = []
+
+c = mai = men = 0
 
 while True:
-    lista.append([input('Nome: '), input('Peso: ')])
+   nome = input('Nome: ')
+   peso = int(input('Peso:'))
+
+
+    lista.append([nome, peso])
     r = ' '
+
+    if len(lista) == 0:
+        mai = men = lista[0][1]
+    if peso < men:
+        men = peso
+    if peso > mai:
+        mai = peso
+
     while r not in 'sSnN':
         r = input('Deseja continuar? ').strip()
     if r in 'nN':
         break
 
-print(f'Foram cadastradas {len(lista)+1} pessoas')
+for p in lista:
+    if p[1] == men:
+        leves.append(p[0])
+    if p[1] == mai:
+        pesados.append(p[0])
+
+print(f'Foram cadastradas {len(lista)} pessoas')
+print(f'As pessoas mais leve são:{leves}')
+print(f'As pessoas mais pesadas são: {pesados}')
